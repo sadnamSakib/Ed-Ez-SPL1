80f1ffed0ad3d4648034ff60cf275ffc67b90cbf
Mirza Mohammad Azwad <50703605+mirzaazwad@users.noreply.github.com>
2022-09-21 16:39:17 +0600

    Adding DDL files for modified database

--- code/database/user.sql
+++ code/database/user.sql
@@ -2,8 +2,8 @@
 -- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Sep 11, 2022 at 07:55 AM
+-- Host: localhost
+-- Generation Time: Sep 21, 2022 at 12:38 PM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.6
 
@@ -33,7 +33,12 @@ CREATE TABLE `student` (
   `password` text NOT NULL,
   `dob` date NOT NULL,
   `gender` text NOT NULL,
-  `institution` text NOT NULL
+  `institution` text NOT NULL,
+  `ProfilePicture` longblob DEFAULT NULL,
+  `MobileNumber` text DEFAULT NULL,
+  `Department` text DEFAULT NULL,
+  `Semester` int(11) DEFAULT NULL,
+  `Country` text DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -44,11 +49,16 @@ CREATE TABLE `student` (
 
 CREATE TABLE `teacher` (
   `username` text NOT NULL,
-  `email` varchar(200) NOT NULL,
+  `email` varchar(250) NOT NULL,
   `password` text NOT NULL,
   `dob` date NOT NULL,
   `gender` text NOT NULL,
-  `institution` text NOT NULL
+  `institution` text NOT NULL,
+  `ProfilePicture` longblob DEFAULT NULL,
+  `MobileNumber` text DEFAULT NULL,
+  `Country` text DEFAULT NULL,
+  `Department` text DEFAULT NULL,
+  `Designation` text DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
