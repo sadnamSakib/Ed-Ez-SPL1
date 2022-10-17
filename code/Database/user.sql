e1dfa68394d825afaad916e69234f42122fb4d4b
Mirza Mohammad Azwad <50703605+mirzaazwad@users.noreply.github.com>
2022-10-17 13:51:30 +0600

    Changes Made

--- code/Database/user.sql
+++ code/Database/user.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 16, 2022 at 06:25 PM
+-- Generation Time: Oct 17, 2022 at 09:51 AM
 -- Server version: 10.4.25-MariaDB
 -- PHP Version: 8.1.10
 
