c974e8b4c804f9feda988230e3c26b8177f4a73b
Sadnam Sakib <sadnamsakib@iut-dhaka.edu>
2022-10-23 20:30:43 +0600

    Adding boxicon locally to student user.

--- code/UserProfiles/StudentProfile/StudentSchedule/index.php
+++ code/UserProfiles/StudentProfile/StudentSchedule/index.php
@@ -49,7 +49,7 @@ foreach ($classrooms as $dummy_classroom) {
   <link rel="stylesheet" href="main.min.css" />
   <link rel="stylesheet" href="<?php echo $root_path; ?>css/bootstrap.css" />
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-  <link href="https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css" rel="stylesheet" />
+  <link href="<?php echo $root_path;?>boxicons-2.1.4/css/boxicons.min.css" rel="stylesheet" />
   <script defer src="script.js"></script>
 
 </head>
