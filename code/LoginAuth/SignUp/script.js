a31ec005bf565a3e9e789427cf40c1146a89f51b
Zaara Zabeen Arpa <95168733+zaarpa@users.noreply.github.com>
2022-10-25 12:15:14 +0600

    Changing password validation message

--- code/LoginAuth/SignUp/script.js
+++ code/LoginAuth/SignUp/script.js
@@ -73,12 +73,12 @@ function passwordVerification() {
             }
         }
         if (charPresentSmall === false || charPresentBig === false || numPresent === false || symbolPresent === false) {
-            password_message.push("Password must contain minimum of 8 characters which must include numbers, letters of both cases and symbols")
+            password_message.push("Password must contain minimum of 8 characters including numbers, letters of both cases and symbols")
         }
 
     }
     else {
-        password_message.push('Password must contain minimum of 8 characters which must include numbers, letters of both cases and symbols')
+        password_message.push('Password must contain minimum of 8 characters including numbers, letters of both cases and symbols')
     }
     if (password_message.length > 0) {
         errorPassword.style.display = "block"
