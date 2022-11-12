c10efea17160a6edf9db161c1b89f98dbc482145
Sadnam Sakib <sadnamsakib@iut-dhaka.edu>
2022-11-12 17:20:47 +0600

    Adding douhgnut chart to index page

--- code/UserProfiles/StudentProfile/GradingSystem/index.php
+++ code/UserProfiles/StudentProfile/GradingSystem/index.php
@@ -11,7 +11,8 @@ $tableName = $_SESSION['tableName'];
 $email = new EmailValidator($_SESSION['email']);
 $validate = new InputValidation();
 $classCode = $_SESSION['class_code'];
-$classrooms = $database->performQuery("SELECT * FROM classroom,student_classroom where classroom.class_code=student_classroom.class_code and student_classroom.email='" . $email->get_email() . "' and active='1';");
+$task =
+  $classrooms = $database->performQuery("SELECT * FROM classroom,student_classroom where classroom.class_code=student_classroom.class_code and student_classroom.email='" . $email->get_email() . "' and active='1';");
 ?>
 
 <!DOCTYPE html>
@@ -65,7 +66,58 @@ $classrooms = $database->performQuery("SELECT * FROM classroom,student_classroom
 
     </section>
   </div>
+  <script>
+    var myChartCircle = new Chart('chartProgress', {
+      type: 'doughnut',
+      data: {
+        datasets: [{
+          label: 'Total percentage',
+          percent: (50 * 100) / 75,
+          backgroundColor: ['#2f6d8b']
+        }]
+      },
+      plugins: [{
+          beforeInit: (chart) => {
+            const dataset = chart.data.datasets[0];
+            chart.data.labels = [dataset.label];
+            dataset.data = [dataset.percent, 100 - dataset.percent];
+          }
+        },
+        {
+          beforeDraw: (chart) => {
+            var width = chart.chart.width,
+              height = chart.chart.height,
+              ctx = chart.chart.ctx;
+            ctx.restore();
+            var fontSize = (height / 90).toFixed(2);
+            ctx.font = fontSize + "em sans-serif";
+            ctx.fillStyle = "#9b9b9b";
+            ctx.textBaseline = "middle";
 
+
+
+            var text = chart.data.datasets[0].percent.toFixed(2);
+            textX = Math.round((width - ctx.measureText(text).width) / 2.2),
+              textY = height / 2;
+            ctx.fillText(text + "%", textX, textY);
+            ctx.save();
+          }
+        }
+      ],
+      options: {
+        maintainAspectRatio: false,
+        aspectRatio: 1,
+        cutoutPercentage: 80,
+        rotation: Math.PI / 2,
+        legend: {
+          display: false,
+        },
+        tooltips: {
+          filter: tooltipItem => tooltipItem.index == 0
+        }
+      }
+    });
+  </script>
 </body>
 
 </html>
