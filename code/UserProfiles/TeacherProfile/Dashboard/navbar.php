67dadd81fd6d141cd0e204493f0f1da63d2bd7de
Zaara Zabeen Arpa <95168733+zaarpa@users.noreply.github.com>
2022-12-22 20:22:57 +0600

    Dashboard for teacher

---
+++ code/UserProfiles/TeacherProfile/Dashboard/navbar.php
@@ -0,0 +1,11 @@
+<?php 
+    require $root_path.'UserProfiles/Navbar/index.php';
+    function student_navbar($root_path,$profile=false){
+        if($profile){
+            navbar($root_path.'UserProfiles/StudentProfile/',$root_path.'UserProfiles/StudentProfile/index.php',$root_path.'UserProfiles/StudentProfile/StudentSchedule/index.php',$root_path.'UserProfiles/StudentProfile/ClassroomSystem/index.php',$root_path.'UserProfiles/StudentProfile/GradingSystem/index.php',$root_path,$root_path.'UserProfiles/UpdatePassword/');
+        }
+        else{
+            navbar($root_path.'UserProfiles/StudentProfile/',$root_path.'UserProfiles/StudentProfile/index.php',$root_path.'UserProfiles/StudentProfile/StudentSchedule/index.php',$root_path.'UserProfiles/StudentProfile/ClassroomSystem/index.php',$root_path.'UserProfiles/StudentProfile/GradingSystem/index.php',$root_path);
+        }
+    }
+?>
