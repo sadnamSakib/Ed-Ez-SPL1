3a7bb8f545c97834a36045835e5d6189b71a9707
Sadnam Sakib <sadnamsakib@iut-dhaka.edu>
2022-12-29 02:29:23 +0600

    teacher dashboard dropdown function

--- code/UserProfiles/TeacherProfile/Dashboard/script.js
+++ code/UserProfiles/TeacherProfile/Dashboard/script.js
@@ -115,10 +115,27 @@ window.onload = function () {
   document.body.appendChild(css);
 };
 
+
+function myFunctionsmall() {
+  document.getElementById("myDropdownsmall").classList.toggle("show");
+}
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtnsmall')) {
+    var dropdowns = document.getElementsByClassName("dropdown-contentsmall");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+};
+
+
 function myFunction() {
   document.getElementById("myDropdown").classList.toggle("show");
 }
-
 // Close the dropdown menu if the user clicks outside of it
 window.onclick = function(event) {
   if (!event.target.matches('.dropbtn')) {
